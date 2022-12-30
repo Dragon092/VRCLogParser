0a858e05fd95612e83ef31fea0797c0693ecc72c
Kevin Birke <mail@kevinbirke.de>
2022-12-30 06:42:55 +0100

    Add line parsing

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,34 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(527, 55);
+            this.button1.Location = new System.Drawing.Point(635, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(153, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(617, 424);
+            this.listBox1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +67,6 @@
         #endregion
 
         private Button button1;
+        private ListBox listBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 namespace VRCLogParser
 {
     using System.IO;
+    using System.Windows.Forms;
+    using static System.Net.Mime.MediaTypeNames;
 
     public partial class Form1 : Form
     {
@@ -25,7 +27,12 @@ namespace VRCLogParser
 
         private void parse_file(FileInfo file)
         {
-            System.Diagnostics.Debug.WriteLine("Parsing "+file.Name+"...");
+            System.Diagnostics.Debug.WriteLine($"Parsing {file.Name}...");
+
+            string[] lines = File.ReadAllLines(file.FullName);
+
+            foreach (string line in lines.Where(l => l.Contains("[Video Playback]")))
+                listBox1.Items.Add(line);
         }
     }
 }
