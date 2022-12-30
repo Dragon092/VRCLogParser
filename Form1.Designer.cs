97fc1de152be9c5388881d5d01760f42c584cce1
Kevin Birke <mail@kevinbirke.de>
2022-12-30 07:38:53 +0100

    Add copying links

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -38,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Parse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -51,11 +52,22 @@
             this.listBox1.Size = new System.Drawing.Size(617, 424);
             this.listBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(635, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Copy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -68,5 +80,6 @@
 
         private Button button1;
         private ListBox listBox1;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,9 @@
 namespace VRCLogParser
 {
     using System.IO;
+    using System.Text.RegularExpressions;
     using System.Windows.Forms;
+    using static System.ComponentModel.Design.ObjectSelectorEditor;
     using static System.Net.Mime.MediaTypeNames;
 
     public partial class Form1 : Form
@@ -15,9 +17,16 @@ namespace VRCLogParser
         {
             System.Diagnostics.Debug.WriteLine("Running...");
 
+            listBox1.Items.Clear();
+
             DirectoryInfo d = new DirectoryInfo(@"C:\Users\mail\AppData\LocalLow\VRChat\VRChat");
 
             FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+
+            if(Files.Length > 1)
+            {
+                System.Diagnostics.Debug.WriteLine($"Found more then 1 log file: {Files.Length} found");
+            }
 
             foreach (FileInfo file in Files)
             {
@@ -31,8 +40,23 @@ namespace VRCLogParser
 
             string[] lines = File.ReadAllLines(file.FullName);
 
+            lines = lines.Reverse().ToArray();
+
             foreach (string line in lines.Where(l => l.Contains("[Video Playback]")))
                 listBox1.Items.Add(line);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string txt = listBox1.GetItemText(listBox1.SelectedItem);
+
+            foreach (Match item in Regex.Matches(txt, @"((http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?)"))
+            {
+                System.Diagnostics.Debug.WriteLine($"{item.Value}");
+                //MessageBox.Show(item.Value);
+
+                Clipboard.SetText(item.Value);
+            }
         }
     }
 }
